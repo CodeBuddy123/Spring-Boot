e94df8d330200b718d285e292911e87154333e94
Ashish <hangoutbuddy3@gmail.com>
2019-12-05 07:57:53 +0530

    1.Using Eclipse-Maven to Run SpringBoot app
    1.Using Eclipse-Maven to Run SpringBoot app

---
+++ bootapp01/src/main/java/com/ashsoft/test/Test.java
@@ -0,0 +1,15 @@
+package com.ashsoft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test.class, args);
+		System.out.println("Spring Boot app using Eclipse");
+
+	}
+
+}
