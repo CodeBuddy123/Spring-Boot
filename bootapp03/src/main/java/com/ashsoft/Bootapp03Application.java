52a081b3fef6a75b0b7b52a3e24f806a97761787
Ashish <hangoutbuddy3@gmail.com>
2019-12-05 08:04:57 +0530

    Using Spring Initializr for Boot App
    Using Spring Initializr for Boot App

---
+++ bootapp03/src/main/java/com/ashsoft/Bootapp03Application.java
@@ -0,0 +1,14 @@
+package com.ashsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootapp03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootapp03Application.class, args);
+		System.out.println("This app is created using Spring Initializr");
+	}
+
+}
